adf7e32e29b6a43f8f721e49c2d47d0873f97dcb
jalil <63772680+jalil49@users.noreply.github.com>
2021-03-14 19:08:43 -0500

    Update Settings.cs
    Clear ChaDefaults on maker exit

--- src/Settings.cs
+++ src/Settings.cs
@@ -5,6 +5,7 @@ using Common;
 using KKAPI;
 using KKAPI.Chara;
 using KKAPI.MainGame;
+using KKAPI.Maker;
 namespace Cosplay_Academy
 {
     [BepInPlugin(Guid, "Cosplay Academy", Version)]
@@ -69,7 +70,7 @@ namespace Cosplay_Academy
 
             GameAPI.RegisterExtraBehaviour<GameEvent>("Cosplay Academy");
             CharacterApi.RegisterExtraBehaviour<CharaEvent>("Cosplay Academy: Chara");
-
+            MakerAPI.MakerExiting += MakerAPI_MakerExiting;
             UpdateFrequency = Config.Bind("Main Game", "Update Frequency", OutfitUpdate.Daily);
             EnableDefaults = Config.Bind("Main Game", "Enable Default in rolls", true, "Adds default outfit to roll tables");
             SumRandom = Config.Bind("Main Game", "Use Sum random", false, "Tables are added together and drawn from based on experience. This probably makes lewd outfits rarer. \nDefault based on Random with a cap of heroine experience lewd rolls are guaranteed if heroine lands on lewd roll.");
@@ -110,6 +111,11 @@ namespace Cosplay_Academy
             PermReset = Config.Bind("Maker", "Keep Randomize Sets on", false, "Reset randomize Sets to disable once called");
             ChangeOutfit = Config.Bind("Maker", "Change generated outfit", false);
             PermChangeOutfit = Config.Bind("Maker", "Keep change generated outit on", false, "Reset change generated outfit to disable once called");
+        }
+
+        private void MakerAPI_MakerExiting(object sender, System.EventArgs e)
+        {
+            Constants.ChaDefaults.Clear();
         }
     }
 }
