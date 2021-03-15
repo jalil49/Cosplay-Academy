8ddac8a89c44f92b64fd46ff1a2175b3c1acf5e5
jalil <63772680+jalil49@users.noreply.github.com>
2021-03-14 19:08:57 -0500

    Update Versions.cs

--- src/Versions.cs
+++ src/Versions.cs
@@ -3,6 +3,6 @@
     internal static class Versions
     {
         public const string Guid = "93c2d098-9027-4b04-83a5-ea1fc3e220f8";
-        public const string Version = "0.5.1";
+        public const string Version = "0.5.2";
     }
 }
