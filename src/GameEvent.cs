81f6116f6b52782004b3b923363ee24d1ea7dee7
jalil <63772680+jalil49@users.noreply.github.com>
2021-05-17 04:57:10 -0500

    Readded onstartH to reload at start

--- src/GameEvent.cs
+++ src/GameEvent.cs
@@ -1,7 +1,5 @@
 ﻿using ActionGame;
-using HarmonyLib;
 using KKAPI.MainGame;
-using System.Collections.Generic;
 namespace Cosplay_Academy
 {
     public class GameEvent : GameCustomFunctionController
@@ -10,7 +8,6 @@ namespace Cosplay_Academy
         {
             if (period == Cycle.Type.GotoSchool && ExpandedOutfit.UpdateFrequency.Value == OutfitUpdate.Daily)
             {
-                //Constants.ChaDefaults.Clear();
                 OutfitDecider.Reset = true;
             }
             foreach (var item in Constants.ChaDefaults)
@@ -46,40 +43,39 @@ namespace Cosplay_Academy
             ExpandedOutfit.Logger.LogInfo("Reset has applied");
         }
 
-        //protected override void OnStartH(HSceneProc hSceneProc, bool freeH)
-        //{
-        //    if (freeH && ExpandedOutfit.EnableSetting.Value)
-        //    {
-        //        foreach (var item in Constants.ChaDefaults)
-        //        {
-        //            item.ChaControl.ChangeCoordinateTypeAndReload();
-        //            item.ChaControl.SetAccessoryStateAll(true);
-        //        }
-        //    }
-        //    else if (!freeH && ExpandedOutfit.EnableSetting.Value) //required when starting H from special scenes: 3P, caught playing with self, les
-        //    {
-        //        ClothingLoader clothingLoader = new ClothingLoader();
-        //        List<ChaControl> lstFemale = (List<ChaControl>)Traverse.Create(hSceneProc).Field("lstFemale").GetValue();
-        //        foreach (var item in lstFemale)
-        //        {
-        //            //chacontrols from special scenes do not seem to be the same as the ones from reload as changes don't take effect
-        //            //used to do it in CharaEvent, but it broke with textures after talkng to someone
-        //            var ChaPara = item.fileParam;
+        protected override void OnStartH(HSceneProc hSceneProc, bool freeH)
+        {
+            if (freeH && ExpandedOutfit.EnableSetting.Value)
+            {
+                foreach (var item in Constants.ChaDefaults)
+                {
+                    item.ChaControl.ChangeCoordinateTypeAndReload();
+                }
+            }
+            //else if (!freeH && ExpandedOutfit.EnableSetting.Value) //required when starting H from special scenes: 3P, caught playing with self, les
+            //{
+            //    ClothingLoader clothingLoader = new ClothingLoader();
+            //    List<ChaControl> lstFemale = (List<ChaControl>)Traverse.Create(hSceneProc).Field("lstFemale").GetValue();
+            //    foreach (var item in lstFemale)
+            //    {
+            //        //chacontrols from special scenes do not seem to be the same as the ones from reload as changes don't take effect
+            //        //used to do it in CharaEvent, but it broke with textures after talkng to someone
+            //        var ChaPara = item.fileParam;
 
-        //            var ThisOutfitData = Constants.ChaDefaults.Find(x => ChaPara.personality == x.Personality && x.FullName == ChaPara.fullname && x.BirthDay == ChaPara.strBirthDay);
-        //            if (ThisOutfitData != null)
-        //            {
-        //                //Storedwaifus[ChaPara.fullname + ChaPara.personality + ChaPara.strBirthDay] = ThisOutfitData.ChaControl;
-        //                //ThisOutfitData.ChaControl = item;
-        //                //ThisOutfitData.Chafile = item.chaFile;
-        //                int retain = item.chaFile.status.coordinateType;
-        //                clothingLoader.FullLoad(ThisOutfitData, item, item.chaFile);
-        //                item.chaFile.status.coordinateType = retain;
-        //                item.ChangeCoordinateTypeAndReload();
-        //            }
-        //        }
-        //    }
-        //}
+            //        var ThisOutfitData = Constants.ChaDefaults.Find(x => ChaPara.personality == x.Personality && x.FullName == ChaPara.fullname && x.BirthDay == ChaPara.strBirthDay);
+            //        if (ThisOutfitData != null)
+            //        {
+            //            //Storedwaifus[ChaPara.fullname + ChaPara.personality + ChaPara.strBirthDay] = ThisOutfitData.ChaControl;
+            //            //ThisOutfitData.ChaControl = item;
+            //            //ThisOutfitData.Chafile = item.chaFile;
+            //            int retain = item.chaFile.status.coordinateType;
+            //            clothingLoader.FullLoad(ThisOutfitData, item, item.chaFile);
+            //            item.chaFile.status.coordinateType = retain;
+            //            item.ChangeCoordinateTypeAndReload();
+            //        }
+            //    }
+            //}
+        }
 
         protected override void OnEndH(HSceneProc hSceneProc, bool freeH)
         {
